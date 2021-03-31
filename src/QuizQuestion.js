1096dd3b4374b29ae5d8f9a18785e06449d8fb83
AC75097548 <laurent.botella@capgemini.com>
2021-03-31 09:56:40 +0200

    remove constructor

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,10 +3,6 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <main>
