40d24207df3c5da3905a9e14e32d9351c5935484
AC75097548 <laurent.botella@capgemini.com>
2021-03-31 10:29:35 +0200

    quiz end component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import QuizQuestion from "./QuizQuestion.js";
+import QuizEnd from "./QuizEnd.js";
 
 let quizData = require("./quiz_data.json");
 
@@ -14,11 +15,16 @@ class Quiz extends Component {
   }
 
   render() {
-    return (
-      <QuizQuestion
-        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-      />
-    );
+    const isQuizEnd = this.state.quiz_position === 1 ? true : false;
+    if (isQuizEnd) {
+      return <QuizEnd />;
+    } else {
+      return (
+        <QuizQuestion
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        />
+      );
+    }
   }
 }
 
